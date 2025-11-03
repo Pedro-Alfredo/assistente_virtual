a792b0ae915caa62654a9f81c2ce2935a9610bf7
Pedro Alfredo <cuinhanepedro250@gmail.com>
2025-11-03 12:46:42 +0200

    Fix syntax error in HUGGINGFACE_TOKEN assignment

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 import os
 
 # Token Hugging Face (substitui pelo teu se necessário)
-HUGGINGFACE_TOKEN = os.getenv("HUGGINGFACE_TOKEN"
+HUGGINGFACE_TOKEN = os.getenv("HUGGINGFACE_TOKEN")
 
 # Modelo gratuito e rápido
 HF_MODEL = "mistralai/Mistral-7B-Instruct-v0.2"
